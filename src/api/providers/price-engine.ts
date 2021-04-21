1a31b558eb3e6dd4e4f9ab52f019cca254d87c44
khoa <khoatran.kt1991@gmail.com>
2021-04-22 01:04:34 +0700

    Price engine slash commands build

---
+++ src/api/providers/price-engine.ts
@@ -0,0 +1,47 @@
+import axios from "axios"
+
+const PRICE_ENGINE_URL = 'http://pricing.oxstreet.com/api';
+
+const ROW_LENGTH = 10;
+const fillStr = (str: string): string => ' '.repeat(ROW_LENGTH - str.length) + str;
+
+export const formatResultTable = (data: Array<any>) => {
+  return data.map(e => fillStr(e.size) + fillStr(e.oxstreet) + fillStr(e.goat) + fillStr(e.stockx)).join('\n');
+}
+
+export const getPrices = async (str: string) => {
+  const [sku, currency ] = str.split(' ');
+  const url = `${PRICE_ENGINE_URL}/${sku.toUpperCase()}/prices${currency ? `?currency=${currency.toUpperCase()}`: ``}`;
+  console.log(`calling url ${url}`);
+  const { data } = await axios.get(url);
+  console.log('result data', data);
+  return data;
+}
+
+export const getOffers = async (str: string) => {
+  const [sku, currency ] = str.split(' ');
+  const url = `${PRICE_ENGINE_URL}/${sku.toUpperCase()}/offers${currency ? `?currency=${currency.toUpperCase()}`: ``}`;
+  console.log(`calling url ${url}`);
+  const { data } = await axios.get(url);
+  console.log('result data', data);
+  return data;
+}
+
+
+export const getPrice = async (str: string) => {
+  const [sku, size, currency ] = str.split(' ');
+  const url = `${PRICE_ENGINE_URL}/${sku.toUpperCase()}/${size}/price${currency ? `?currency=${currency.toUpperCase()}`: ``}`;
+  console.log(`calling url ${url}`);
+  const { data } = await axios.get(url);
+  console.log('result data', data);
+  return data;
+}
+
+export const getOffer = async (str: string) => {
+  const [sku, size, currency ] = str.split(' ');
+  const url = `${PRICE_ENGINE_URL}/${sku.toUpperCase()}/${size}/offer${currency ? `?currency=${currency.toUpperCase()}`: ``}`;
+  console.log(`calling url ${url}`);
+  const { data } = await axios.get(url);
+  console.log('result data', data);
+  return data;
+}
--- src/api/routes/slack.ts
+++ src/api/routes/slack.ts
@@ -2,14 +2,14 @@
 import express, { Router, Request, Response, NextFunction } from 'express';
 const router: Router = express.Router();
 import { sendMessage, sendMarkdownMessage } from '../providers/slack';
-
+import { getPrices, getOffers, getPrice, getOffer } from '../providers/price-engine';
 const ROUTE_NAME = 'slack';
 /**
  * GET METHOD
  */
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-  const { message, channel } = req.query as any;
-  sendMessage(channel, message);
+  const { message, channel_id = 'testbot' } = req.query as any;
+  sendMessage(channel_id, message);
   res.status(200).json({
     message: `Handling GET requests to /${ROUTE_NAME} success`
   })
@@ -46,13 +46,88 @@ router.post('/', (req: Request, res: Response, next: NextFunction) => {
  */
  router.post('/read-my-request', (req: Request, res: Response, next: NextFunction) => {
   console.log('data', req.body);
-  const { channel, text } = req.body as any;
-  sendMessage(channel, `I got your command ${text}`);
+  const { channel_id = 'testbot', text } = req.body as any;
+  sendMessage(channel_id, `I got your command ${text}`);
   res.status(200).json({
     message: `Handling POST requests to /${ROUTE_NAME}`,
     ...req.body
   })
 });
+
+router.post('/prices', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log('prices data', req.body);
+    const { channel_id = 'testbot', text } = req.body as any;
+    console.log('channel_id', channel_id);
+    sendMessage(channel_id, `Getting Prices from ${text}`);
+    const result = await getPrices(text);
+    sendMessage(channel_id, JSON.stringify(result));
+    res.status(200).json({
+      message: `Handling POST requests to /${ROUTE_NAME}`,
+      ...req.body
+    })
+  } catch (error) {
+    console.log('error', error);
+    const { message } = error;
+    res.status(400).send({ message });
+  }
+});
+
+router.post('/price', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log('prices data', req.body);
+    const { channel_id = 'testbot', text } = req.body as any;
+    sendMessage(channel_id, `Getting Price from ${text}`);
+    const result = await getPrice(text);
+    sendMessage(channel_id, JSON.stringify(result));
+    res.status(200).json({
+      message: `Handling POST requests to /${ROUTE_NAME}`,
+      ...req.body
+    })
+  } catch (error) {
+    console.log('error', error);
+    const { message } = error;
+    res.status(400).send({ message });
+  }
+});
+
+router.post('/offers', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log('prices data', req.body);
+    const { channel_id = 'testbot', text } = req.body as any;
+    sendMessage(channel_id, `Getting Offers from ${text}`);
+    const result = await getOffers(text);
+    sendMessage(channel_id, JSON.stringify(result));
+    res.status(200).json({
+      message: `Handling POST requests to /${ROUTE_NAME}`,
+      ...req.body
+    })
+  } catch (error) {
+    console.log('error', error);
+    const { message } = error;
+    res.status(400).send({ message });
+  }
+});
+
+router.post('/offer', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log('prices data', req.body);
+    const { channel_id = 'testbot', text } = req.body as any;
+    sendMessage(channel_id, `Getting Offer from ${text}`);
+    const result = await getOffer(text);
+    sendMessage(channel_id, JSON.stringify(result));
+    res.status(200).json({
+      message: `Handling POST requests to /${ROUTE_NAME}`,
+      ...req.body
+    })
+  } catch (error) {
+    console.log('error', error);
+    const { message } = error;
+    res.status(400).send({ message });
+  }
+});
+
+
 
 
 
